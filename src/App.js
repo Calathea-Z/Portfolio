b4bb27ccc900e22575e80eaa063abc4f6634b668
Calathea-Z <zsykes21@gmail.com>
2022-12-12 15:34:11 -0500

    Update all components so they properly render.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
 
+// IMPORT COMPONENTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
-
+// IMPORT PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
 function App() {
-  const URL = "#"
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/";
+
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route exact path="/" element={ <Home/> } />
-        <Route path="/projects" element={ <Projects URL={ URL } /> } />
-        <Route path="/about" element={ <About URL={ URL } /> } />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,34 +1,43 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function Projects(props) {
+  // create state to hold projects
+  const [projects, setProjects] = useState(null);
+
+  //create function to make api call
+  const getProjectsData = async () => {
     
-    const  [projects, setProjects] = useState(null);
+		//make api call and get response
+    const response = await fetch("./projects.json");
+    
+		// turn response into javascript object
+    const data = await response.json();
+    
+		// set the projects state to the data
+    setProjects(data);
 
-    const getProjectsData = async () => {
-        const response = await fetch("./projects.json");
+  };
 
-        const data = await response.json();
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => getProjectsData(), []);
 
-        setProjects(data);
-    };
-
-    useEffect(() => getProjectsData(), [])
-
-    const loaded = () => {
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => {
     return projects.map((project) => (
-        <div>
-            <h1>{project.name}</h1>
-            <img src={project.image} />
-            <a href={project.git}>
-                <button>Github</button>
-            </a>
-            <a href={project.live}>
-            <button>live site</button>
-            </a>
-        </div>      
-));
-};
-    return ( projects ? loaded() : <h1>Loading...</h1>)
+      <div>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        <a href={project.live}>
+          <button>live site</button>
+        </a>
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
 }
 
-export default Projects
+export default Projects;
