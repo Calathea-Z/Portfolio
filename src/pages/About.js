66f54b82aca706eaec7fbe41fde62f18a2a6ead5
Calathea-Z <zsykes21@gmail.com>
2022-12-12 15:21:14 -0500

    frontend complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function About(props) {
-    return (
-        <h1>About</h1>
-    )
+
+    const [about, setAbout] = useState(null);
+
+    const getAboutData = async () => {
+        const response = await fetch("./about.json");
+        console.log(response);
+
+        const data = await response.json();
+        console.log(data);
+        setAbout(data);
+    }
+
+    useEffect(() => getAboutData(), []);
+
+    const loaded = () => {
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    };
+    return ( about ?  loaded() : <h1>Loading..</h1> )
 }
 
-export default About
+export default About;
