c6ce5a4a270d3c3db68bd231e20811b44f3a253c
Calathea-Z <zsykes21@gmail.com>
2022-12-17 12:42:26 -0500

    Finish css for about.jsx component including media querys.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,36 +11,37 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className='container about__container'>
-        <div className="about__me">
-          <img src={suziePhoto} alt='Alternate Profile Image'/>
+        <div className='about__me'>
+          <div className='about__me-img'>
+            <img src={suziePhoto} alt='Alternate Profile Image'/>
+          </div>
         </div>
-      </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>1 Year</small>
+            </article>
 
-      <div className='about__content'>
-        <div className='about__cards'>
-          <article className='about__cards'>
-            <FaAward className='about__icon' />
-            <h5>Experience</h5>
-            <small>1 Year</small>
-          </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>2+</small>
+            </article>
 
-          <article className='about__cards'>
-            <FiUsers className='about__icon' />
-            <h5>Clients</h5>
-            <small>1 Year</small>
-          </article>
+            <article className='about__card'>
+              <RiFolder2Line className='about__icon' />
+              <h5>Projects</h5>
+              <small>3+</small>
+           </article>
 
-          <article className='about__cards'>
-            <RiFolder2Line className='about__icon' />
-            <h5>Projects</h5>
-            <small>1 Year</small>
-          </article>
+          </div>
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis odit nesciunt nostrum! Qui veniam ea quasi, sequi laboriosam odio error accusantium deserunt, maiores eum dolorum illum, quaerat quam optio non!
+          </p>
+          
+          <a href='contact' className='btn btn-primary'>Let's Talk</a>
         </div>
-
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis odit nesciunt nostrum! Qui veniam ea quasi, sequi laboriosam odio error accusantium deserunt, maiores eum dolorum illum, quaerat quam optio non!
-        </p>
-
-        <a href='contact' className='btn btn-primary'>Let's Talk</a>
       </div>
     </section>
   )
