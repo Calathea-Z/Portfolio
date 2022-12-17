742833c59a8dd35b6f8d86b7d2f7704f9ca5fc94
Calathea-Z <zsykes21@gmail.com>
2022-12-17 18:28:31 -0500

    Build Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,21 @@
 import '../styles/footer.css'
+import { FiInstagram } from 'react-icons/fi'
 
 const Footer  = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://instagram.com'>< FiInstagram /> </a>
+      </div>
+    </footer>
   )
 }
 
