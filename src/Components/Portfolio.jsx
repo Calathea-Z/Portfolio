4eff80d897024048dae0324f40ffe03f188df67a
Calathea-Z <zsykes21@gmail.com>
2022-12-17 17:14:21 -0500

    Add photos of 2 projects. Update portfolio.css and Portfolio.jsx. Added dynamic map for ease of addition of futur e projects.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,9 +1,50 @@
 import '../styles/portfolio.css'
+import BlackjackIMG from '../assets/blackjack_ui.png'
+import RandomDogIMG from '../assets/randomdog_ui.png'
+
+const data = [
+  {
+    id: 1,
+    image: BlackjackIMG,
+    title: 'Blackjack (JavaScript)',
+    github: 'https://github.com/Calathea-Z/black_jack',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: RandomDogIMG,
+    title: 'Random Dog App (REACT)',
+    github: 'https://github.com/Calathea-Z/project-two-react-app',
+    demo: 'https://github.com' 
+  }
+]
+
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={ id } className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={ image } alt={title} />
+          </div>
+          <h3>{ title }</h3>
+          <div className="portfolio__item-cta">
+            <a href={ github } className='btn'>GitHub</a>
+            <a href={ demo } className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+            )
+          })
+        }
+        
+      </div>
     </section>
   )
 }
