3d9945cac7fd9e7646abc56b0995b4a9b785bdd2
Calathea-Z <zsykes21@gmail.com>
2022-12-17 09:40:36 -0500

    Fix header button colors.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,19 @@
 import '../styles/header.css'
+import Resume from '../Components/Resume'
 
 const Header= () => {
   return (
     <div>
-      Header
+      <header>
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1> Zach Sykes</h1>
+          <div className="text-light">
+            FullStack Developer
+          </div>
+          <Resume />
+        </div>
+      </header>
     </div>
   )
 }
---
+++ src/Components/Resume.jsx
@@ -0,0 +1,12 @@
+import resume from '../assets/resume.pdf' 
+
+const Resume = () => {
+  return (
+    <div className='cta'>
+      <a href={resume} download className="btn">Download Resume</a>
+      <a href='#contact' className="btn btn-primary">Let's Talk!</a>
+    </div>
+  )
+}
+
+export default Resume
