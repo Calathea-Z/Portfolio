253e04f38d5451d6f423b30487934b908d3c7434
Calathea-Z <zsykes21@gmail.com>
2022-12-17 18:21:39 -0500

    Add emailjs functionality. Update CSS for mobile responsivness. Add email link.

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
 import '../styles/contact.css'
 import {HiOutlineMail} from 'react-icons/hi'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser'
+
 
 const Contacts = () => {
+
+  const form = useRef();
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs.sendForm('service_70igmt9', 'template_i71krge', form.current, 'CdsvoqzAtZyIJxv7f').then((result) => {
+      console.log(result.text);
+  }, (error) => {
+      console.log(error.text);
+  });
+  event.target.reset();
+};
+
+
   return (
     <section id='contact'>
      <h5>Get In Touch</h5>
@@ -9,19 +28,20 @@ const Contacts = () => {
      <div className='container contact__container'>
       <div className='contact__options'>
         <article className='contact__option'>
-          < HiOutlineMail />
+          < HiOutlineMail className='contact__option-icon'/>
           <h4>Email</h4>
           <h5>zsykes21@gmail.com</h5>
           <a href='mailto:zsykes21@gmail.com'>Send a message</a>
         </article>
       </div>
+     
+      <form ref={ form } onSubmit={ sendEmail }>
+        <input type='text' name='name' placeholder='Your Full Name' required />
+        <input type='email' name='email' placeholder='Your Email' required />
+        <textarea name='message' rows='7' placeholder='Your Message' required />
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
      </div>
-     <form action=''>
-      <input type='text' name='name' placeholder='Your Full Name' required />
-      <input type='email' name='email' placeholder='Your Email' required />
-      <textarea name='message' rows='7' placeholder='Your Message' required />
-      <button type='submit' className='btn btn-primary'>Send Message</button>
-     </form>
     </section>
   )
 }
